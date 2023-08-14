c2cf825555dc81d94e34b4145d73c19d6a3d6ccd
Zain5431 <75997252+Zain5431@users.noreply.github.com>
2023-08-14 15:25:13 +0530

    custom.js

--- custom.js
+++ custom.js
@@ -1,18 +1,11 @@
 // Create the popup element
-var popup = document.createElement('div');
-popup.id = 'popup';
-popup.style.display = 'none';
-popup.innerHTML = '<p>This is a popup!</p>';
-document.body.appendChild(popup);
-
-// Function to show the popup and then hide it after 5 seconds
 function showAndHidePopup() {
-  popup.style.display = 'block';
+    var popup = document.getElementById('popup');
+    popup.style.display = 'block';
+    
+    setTimeout(function() {
+      popup.style.display = 'none';
+    }, 5000); // 5 seconds in milliseconds
+  }
 
-  setTimeout(function() {
-    popup.style.display = 'none';
-  }, 5000); // 5 seconds in milliseconds
-}
-
-// Call the showAndHidePopup function after 20 seconds
-setTimeout(showAndHidePopup, 20000); // 20 seconds in milliseconds
+  setTimeout(showAndHidePopup, 20000); // 20 seconds in milliseconds
